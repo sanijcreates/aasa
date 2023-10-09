66435621b15d7e8a67da62f92ca403f6f039b7e0
Sanij Shrestha <saniz.sth123@gmail.com>
2023-10-08 23:03:11 -0400

    onTouch gesture added

--- app/src/main/java/com/example/gesturetest/MainActivity.kt
+++ app/src/main/java/com/example/gesturetest/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.gesturetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
+    private val DEBUG_TAG = "Gestures"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d(DEBUG_TAG, "Action was Down")
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d(DEBUG_TAG, "Action was MOVE")
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d(DEBUG_TAG, "Action was UP")
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.d(DEBUG_TAG, "Action was CANCEL")
+                true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                Log.d(DEBUG_TAG, "Movement occurred outside bounds of current screen element")
+                true
+            }
+            else -> super.onTouchEvent(event)
+        }
     }
 }
